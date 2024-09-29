4e16404591f0e0ddb056f6a4cbbd570e73a18a01
Suman Mondal <91243593+SM76020@users.noreply.github.com>
2024-09-29 21:07:54 +0530

    Update FoodItems.jsx

--- user-frontend/src/components/FoodItems.jsx
+++ user-frontend/src/components/FoodItems.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FoodItems = ({ item, onAddToCart }) => {
   return (
     <div className="meal-item">
-      <img src={`http://localhost:3000/${item.image}`} alt={item.name} />
+      <img src={`https://food-order-backend-u5gp.onrender.com/${item.image}`} alt={item.name} />
       <h3>{item.name}</h3>
       <p className='meal-item-description'>{item.description}</p>
       <p className='meal-item-price'>Price: ${parseFloat(item.price).toFixed(2)}</p>
